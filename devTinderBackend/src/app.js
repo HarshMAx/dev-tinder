5cf53d2bfda8c250f26c70307af8f8b8cb55b9c5
harshMAx <harshsolanki4121@gmail.com>
2026-04-25 11:24:21 +0530

    create a home url

--- devTinderBackend/src/app.js
+++ devTinderBackend/src/app.js
@@ -5,6 +5,10 @@ const app = express();
 app.use('/',(req,res)=>{
     res.send("Hello from server");
 })
+
+app.use('/home',(req,res)=>{
+    res.send("this is home")
+})
 const PORT = 7777;
 app.listen(PORT,()=>{
     console.log(`Server is running on ${PORT}`);
